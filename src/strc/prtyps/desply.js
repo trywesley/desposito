0d07370e169481d207ac933226a93068217fcb40
trywesley <66664225+trywesley@users.noreply.github.com>
2021-01-02 12:21:08 -0400

    Update desply.js

--- src/strc/prtyps/desply.js
+++ src/strc/prtyps/desply.js
@@ -3,9 +3,14 @@ const phrases = require("../../../phrases/messages.js")
 
 class PROTO {
     static load() {
-        Discord.Message.prototype.desply = function send(ind, ref, ...args) {
+        Discord.Message.prototype.desply = function reply(ind, ref, ...args) {
             const msg = phrases(ind, ref)
             return this.channel.send(msg)
+        }
+
+        Discord.Message.prototype.desdit = function edit(ind, ref, ...args) {
+            const msg = phrases(ind, ref)
+            return this.edit(msg)
         }
     }
 }

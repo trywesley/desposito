89129b628ec49fc71ebc097a8f454ee39cf7c7cf
trywesley <66664225+trywesley@users.noreply.github.com>
2021-01-02 12:22:49 -0400

    Update DespositoClient.js

--- src/utils/discord/DespositoClient.js
+++ src/utils/discord/DespositoClient.js
@@ -1,5 +1,5 @@
 const { Client } = require("discord.js")
-const PROTO = require("../../strc/prtyps/desply")
+const PROTO = require("../../strc/prtyps/proto")
 
 module.exports = class DespoitoClient extends Client {
 	constructor(token) {

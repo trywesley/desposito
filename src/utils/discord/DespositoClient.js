e73f5bc640473b73ca4dc24756c89f0589a10c53
trywesley <66664225+trywesley@users.noreply.github.com>
2021-01-26 13:40:34 -0400

    Update DespositoClient.js

--- src/utils/discord/DespositoClient.js
+++ src/utils/discord/DespositoClient.js
@@ -12,6 +12,18 @@ module.exports = class DespoitoClient extends Client {
 
                 PROTO.load()
 	}
+        
+        resolveCommand (arg) { 
+           const probableCommand = desposito.commands.get(arg) || desposito.aliases.get(arg)
+           if(typeof probableCommand === "string") {
+               const realCommand = desposito.commands.get(probableCommand)
+               realCommand.name = probableCommand
+               return realCommand
+           } else if(typeof probableCommand === "object") {
+               probableCommand.name = arg
+               return probableCommand
+           }
+        }
 
 	get rssUsage () {
 		return Math.round(process.memoryUsage().rss / 1024 / 1024)

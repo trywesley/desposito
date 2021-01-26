3353f17b4248fe9fa33afbcd823dc896c758c1b2
trywesley <66664225+trywesley@users.noreply.github.com>
2021-01-26 15:01:42 -0400

    Update DespositoClient.js

--- src/utils/discord/DespositoClient.js
+++ src/utils/discord/DespositoClient.js
@@ -7,24 +7,11 @@ module.exports = class DespoitoClient extends Client {
 		super.login(token)
                 
 		this.acess = ["748320609746026607", "451920956768649226"]
-		this.commands = new Map()
-                this.aliases = new Map()
+		this.commands = {}
 
                 PROTO.load()
 	}
         
-        resolveCommand (arg) { 
-           const probableCommand = this.commands.get(arg) || this.aliases.get(arg)
-           if(typeof probableCommand === "string") {
-               const realCommand = this.commands.get(probableCommand)
-               realCommand.name = probableCommand
-               return realCommand
-           } else if(typeof probableCommand === "object") {
-               probableCommand.name = arg
-               return probableCommand
-           }
-        }
-
 	get rssUsage () {
 		return Math.round(process.memoryUsage().rss / 1024 / 1024)
 	}

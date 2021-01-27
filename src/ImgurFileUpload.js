cf5ac24d77753fe8c23510a46b94b11a08a44075
weberww0 <60146862+weberww0@users.noreply.github.com>
2021-01-27 19:40:15 -0400

    Create ImgurFileUpload.js

---
+++ src/ImgurFileUpload.js
@@ -0,0 +1,13 @@
+module.exports = function upload(url) => {
+    const data = fetch("https://api.imgur.com/3/image", {
+        method: 'POST',
+        headers: { 
+            'Authorization': "Client-ID " + process.env.IMGUR_CLIENT_ID
+        },
+        body: url
+    })
+    .then (res => res.json())
+    .then(js => js.data)
+     
+     return data.url
+}

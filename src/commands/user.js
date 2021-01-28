7bee9fffcf4f76255a5d003d213f99d8a42e5414
weberww0 <60146862+weberww0@users.noreply.github.com>
2021-01-28 13:15:17 -0400

    Create user.js

---
+++ src/commands/user.js
@@ -0,0 +1,50 @@
+const DespositoEmbed = require("../utils/discord/DespositoEmbed")
+
+module.exports = {
+    aliase: "userinfo ui",
+    countdown: 5,
+    clientPermissions: ["MANAGE_MESSAGES"],
+    async open (data, desposito) {
+    
+        let timeout
+        const user = data.message.arguments[0] ? desposito.users.cache.get(data.message.arguments[0].replace(/[!@<>]/g, "")) : data.message.author
+        if(!user) return data.message.desply("general.no_user")
+
+        const embed = new DespositoEmbed()
+        .selectPreset("userinfo", user)
+
+        const message = await data.message.channel.send(embed)
+        const emoji = "🖼"
+        message.react(emoji)
+
+        const filter = (r, u) => r.emoji.name === emoji && u.id === data.message.author.id
+        const collector = message.createReactionCollector(filter, {time: 60000})
+        collector.on("collect", async (r, u) => {
+            r.remove()
+            let avatars = await desposito.database.get("Desposito/Users/" + user.id + "/Avatars")
+            if(!avatars) {
+                embed.setDescription("Nenhum avatar encontrado.")
+                message.edit(embed)
+            }
+            avatars = Object.values(avatars)
+    
+            let page = 0
+            embed.setImage(avatars[0])
+            message.edit(embed)
+
+            const reactionsArray = ["➖", "➕"]
+            reactionsArray.forEach(r => message.react(r))
+
+            const filter = (r, u) => reactionsArray.includes(r.emoji.name) && u.id === data.message.author.id
+            const collector = message.createReactionCollector(filter)
+            collector.on("collect", (reaction, user) => {
+                if(reaction.emoji.name ===  "➕") { page++ } else { page-- }
+                avatars[page] ? (embed.setImage(avatars[page]), message.edit(embed)) : return
+                if(timeout) clearTimeout(timeout)
+                timeout = setTimeout(() => {
+                    collector.stop()
+                }, 20000)
+            })
+        })
+    }
+}

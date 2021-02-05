d7cb29577d991b9004da4bd7e7067403e8f38311
weberww0 <60146862+weberww0@users.noreply.github.com>
2021-02-05 16:35:15 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const DespositoClient = require("./utils/discord/DespositoClient")
-const desposito = new DespositoClient("ODA3MzE1Njc3OTkyMDU4OTEx.YB2ppQ.yo9pR5outoYIvC83LGu9k400OcQ")
+const desposito = new DespositoClient(process.env.TOKEN)
 
 require("./strc/handlers/command")(desposito)
 require("./strc/handlers/event")(desposito)

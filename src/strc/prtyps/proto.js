baa415bd00ca51b82f428543f84cf4d7fc57aba4
weberww0 <60146862+weberww0@users.noreply.github.com>
2021-02-19 14:31:09 -0400

    Update proto.js

--- src/strc/prtyps/proto.js
+++ src/strc/prtyps/proto.js
@@ -1,20 +1,20 @@
 const Discord = require("discord.js")
-const phrases = require("../../../phrases/messages.js")
+const phrases = require("../../../phrases/manager.js")
 
 class PROTO {
     static load() {
-        Discord.Message.prototype.desply = function reply(ind, ref, ...args) {
+        Discord.Message.prototype.desply = function reply(ind, ref = undefined, ...args) {
             const msg = phrases(ind, ref)
             return this.channel.send(msg)
         }
 
-        Discord.Message.prototype.desdit = function edit(ind, ref, ...args) {
+        Discord.Message.prototype.desdit = function edit(ind, ref = undefined, ...args) {
             const msg = phrases(ind, ref)
             return this.edit(msg)
         }
 
         Discord.Message.prototype.helply = function send(ind, ...args) {
-            return this.channel.send("> " + this.author.username + ", te apresento o comando **" + ind.toUpperCase() + "**!\n> " + phrases("commands." + ind + ".description") + "\n🤔 Não sabe como usar? Use-o da seguinte maneira: `" + phrases("commands." + ind + ".usage") + "`")
+            return this.channel.send("> " + this.author.username + ", te apresento o comando **" + ind.toUpperCase() + "**!\n> " + phrases("help#" + ind + ".description") + "\n🤔 Não sabe como usar? Use-o da seguinte maneira: `" + phrases("help#" + ind + ".usage") + "`")
         }
   
     }

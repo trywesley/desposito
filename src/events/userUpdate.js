7d56d152c0a100b7dd69d6718f16095c41f595fe
weberww0 <60146862+weberww0@users.noreply.github.com>
2021-02-19 00:32:45 -0400

    Update userUpdate.js

--- src/events/userUpdate.js
+++ src/events/userUpdate.js
@@ -1,11 +1,14 @@
-const IFU = require("../utils/ImgurFileUpload")
+const Upload = require("../utils/ImgurFileUpload")
 
 module.exports = (oldUser, newUser, desposito) => {
-    if(oldUser.displayAvatarURL({dynamic: true, size: 2048}) ===  newUser.displayAvatarURL({dynamic: true, size: 2048})) return
-    const obj = {}
-    IFU(newUser.displayAvatarURL({format: "png", dynamic: true, size: 2048})).then(link => {
-        obj[Date.now()] = link 
+    const oldAvatar = oldUser.displayAvatarURL({dynamic: true, size: 2048})
+    const newAvatar = newUser.displayAvatarURL({dynamic: true, size: 2048})  
 
+    if(oldAvatar === newAvatar) return
+    const obj = {}
+
+    Upload(newAvatar).then(link => {
+        obj[Date.now()] = link 
         desposito.database.update("Desposito/Users/" + oldUser.id + "/Avatars", obj)
     })
 }

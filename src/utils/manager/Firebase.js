901cf5961781cf311af42c4119b5dc625ead539b
trywesley <66664225+trywesley@users.noreply.github.com>
2021-02-19 15:12:10 -0400

    Update Firebase.js

--- src/utils/manager/Firebase.js
+++ src/utils/manager/Firebase.js
@@ -1,16 +1,7 @@
 const firebase = require("firebase")
 
 module.exports = () => {
-const fconfig = {
-    apiKey: "AIzaSyCLAobFvnSQcmkBlSqP0LWnC3lQblWag7E",
-    authDomain: "wweberbase.firebaseapp.com",
-    databaseURL: "https://wweberbase.firebaseio.com",
-    projectId: "wweberbase",
-    storageBucket: "wweberbase.appspot.com",
-    messagingSenderId: "876424523824",
-    appId: "1:876424523824:web:dba7529b6b6838fe53fdf5",
-    measurementId: "G-73TD661J43"
-  };
+const fconfig = process.env.FIREBASE_CREDENTIALS
 
 !firebase.apps.length ? firebase.initializeApp(fconfig) : firebase.app()
 return firebase.database()
